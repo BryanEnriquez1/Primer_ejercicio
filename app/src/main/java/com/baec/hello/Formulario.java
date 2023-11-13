f1eeec208f4374787c7270f227d6b11d0d189761
Bryan Enriquez <bryan.enriquez487@ist17dejulio.edu.ec>
2023-11-13 13:12:50 -0500

    VALIDACION DE CAMPOS

--- app/src/main/java/com/baec/hello/Formulario.java
+++ app/src/main/java/com/baec/hello/Formulario.java
@@ -3,6 +3,10 @@ package com.baec.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Formulario extends AppCompatActivity {
 
@@ -10,5 +14,27 @@ public class Formulario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        //CREAR VARIABLES DEL MISMO TIPO DEL LAYOUT
+        Button btValidar = findViewById(R.id.btnvalidar);
+        EditText edNombres, edApellidos, edCedula, edTelefono, edDireccion;
+        edNombres=findViewById(R.id.txtnombre);
+        edApellidos=findViewById(R.id.txtapellido);
+        edCedula=findViewById(R.id.txtcedula);
+        edTelefono=findViewById(R.id.txttelefono);
+        edDireccion=findViewById(R.id.txtdireccion);
+
+        btValidar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nombre=edNombres.getText().toString();
+                if(!nombre.isEmpty()){
+                    Toast.makeText(Formulario.this,"SUBIENDO INFORMACION",Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(Formulario.this,"INGRESE NOMBRE",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
